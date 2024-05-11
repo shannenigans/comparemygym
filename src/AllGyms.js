d4018b8ddb83e42e8168798f1f2f345a3c71e4ae
Shannen Barrameda <sbarrameda17@gmail.com>
2024-05-11 09:27:30 -0700

    render gyms

--- src/AllGyms.js
+++ src/AllGyms.js
@@ -2,25 +2,25 @@ import React from 'react';
 import GymCard from "./GymCard";
 
 export default function AllGyms() {
-    const [ gyms, setGyms ] = React.useState([]);
+    const [gyms, setGyms] = React.useState([]);
 
     React.useEffect(() => {
         fetch('http://localhost:3001/api/getGyms', {
             method: 'GET'
         })
-        .then(res => {
-            return res.json()
-        })
-        .then(gymData => {
-            setGyms(gymData)
-        })
+            .then(res => {
+                return res.json()
+            })
+            .then(gymData => {
+                setGyms(gymData)
+            })
     }, [])
 
-    return (gyms.forEach((gym) => {
-        console.log(gym.name)
-        return (
-            <GymCard name={gym.name} location={gym.location}/>
-        )} )
+    return (
+        <div>
+            {gyms.map((gym, index) => {
+                return <GymCard {...gym} />
+            })}
+        </div>
     )
-    
 }
--- src/GymCard.js
+++ src/GymCard.js
@@ -1,6 +1,7 @@
 import Avatar from '@mui/material/Avatar';
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
+import CardMedia from '@mui/material/CardMedia';
 import CardHeader from '@mui/material/CardHeader';
 import CardContent from '@mui/material/CardContent';
 import CardActions from '@mui/material/CardActions';
@@ -9,34 +10,36 @@ import IconButton from '@mui/material/IconButton';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 
-export default function GymCard({ name, location }) {
+export default function GymCard({ name, location, img }) {
     return (
-        <Box  sx={{ maxWidth: 275 }}>
-        <Card variant="outlined">
-            <CardHeader
-                avatar={
-                    <Avatar aria-label="recipe">
-                        {/* {name[0]} */}
-                    </Avatar>
-                }
-                action={
-                    <IconButton aria-label="settings">
-                        <MoreVertIcon />
+        <Box sx={{ maxWidth: 275 }}>
+            <Card variant="outlined">
+                <CardHeader
+                    avatar={
+                        <Avatar aria-label="avatar name">
+                            {name[0]}
+                        </Avatar>
+                    }
+                    action={
+                        <IconButton aria-label="settings">
+                            <MoreVertIcon />
+                        </IconButton>
+                    }
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                        {name}
+                    </Typography>
+                    <Typography variant="body1">
+                        {location}
+                    </Typography>
+                </CardContent>
+                <CardActions disableSpacing>
+                    <IconButton aria-label="add to favorites">
+                        <FavoriteIcon />
                     </IconButton>
-                }
-            />
-            <CardContent>
-                <Typography>
-                    {name}
-                    {location}
-                </Typography>
-            </CardContent>
-            <CardActions disableSpacing>
-                <IconButton aria-label="add to favorites">
-                    <FavoriteIcon />
-                </IconButton>
-            </CardActions>
-        </Card>
+                </CardActions>
+            </Card>
         </Box>
     )
 }
